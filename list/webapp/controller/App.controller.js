10e569f57001107fbe2b982974f4159357e5f7f6
diegocullera <diegocullera@hotmail.com>
2023-08-10 16:49:17 +0000

    Entrega 2

--- list/webapp/controller/App.controller.js
+++ list/webapp/controller/App.controller.js
@@ -7,9 +7,26 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
+        function myCheck() {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmploye = inputEmployee.getValue();
+
+            if (valueEmploye.length === 6) {
+               // inputEmployee.setDescription("OK");
+               this.getView().byId("labelCountry").setVisible(true);
+               this.getView().byId("slCountry").setVisible(true);
+            } else {
+               // inputEmployee.setDescription("Not OK");
+               this.getView().byId("labelCountry").setVisible(false);
+               this.getView().byId("slCountry").setVisible(false);
+            }
+        }
+
         return Controller.extend("list.controller.App", {
             onInit: function () {
 
-            }
+            },
+
+            onValidate: myCheck
         });
     });
